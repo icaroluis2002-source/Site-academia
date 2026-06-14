8a46e625e8229696cb87e2e2b07f2659af059985
Icaro <icaroluis2002@gmail.com>
2026-06-14 16:12:39 -0300

    Configurado .env e preparado para deploy

--- back-end/database.py
+++ back-end/database.py
@@ -1,9 +1,13 @@
+import os
 import mysql.connector
+from dotenv import load_dotenv
+
+load_dotenv()
 
 def conectar():
     return mysql.connector.connect(
-        host="127.0.0.1",
-        user="root",
-        password="Icaro2310.",
-        database="academia"
+        host=os.getenv("DB_HOST"),
+        user=os.getenv("DB_USER"),
+        password=os.getenv("DB_PASSWORD"),
+        database=os.getenv("DB_NAME")
     )
